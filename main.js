03414971aa1a5796fdaa67fb57dad14ea8bfe0c6
simonefenix <simone.murzilli@fao.org>
2015-06-02 11:22:17 +0200

    updated index

---
+++ main.js
@@ -0,0 +1,93 @@
+define(['jquery',
+        'require',
+        'handlebars',
+        'text!faostat_ui_browse/html/templates.html',
+        'i18n!faostat_ui_browse/nls/translate',
+        'FAOSTAT_UI_COMMONS',
+        'bootstrap',
+        'sweetAlert',
+        'amplify'], function ($, Require, Handlebars, templates, translate, Commons) {
+
+    'use strict';
+
+    function BROWSE() {
+
+        this.CONFIG = {
+            lang: 'en',
+            lang_faostat: 'E',
+            section: null,
+            code: null,
+            datasource: 'faostatdb',
+            placeholder_id: 'faostat_ui_browse',
+
+            // unbind
+            module: null
+        };
+    }
+
+    BROWSE.prototype.init = function(config) {
+
+        /* Extend default configuration. */
+        this.CONFIG = $.extend(true, {}, this.CONFIG, config);
+
+        /* Fix the language, if needed. */
+        this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'en';
+
+        /* Store FAOSTAT language. */
+        this.CONFIG.lang_faostat = Commons.iso2faostat(this.CONFIG.lang);
+
+        /* getting placeholder */
+        var $placeholder = $(this.CONFIG.placeholder_id).length > 0? $(this.CONFIG.placeholder_id): $("#" + this.CONFIG.placeholder_id);
+
+        /* render */
+        this.render($placeholder, this.CONFIG);
+    };
+
+    BROWSE.prototype.render = function($placeholder, config) {
+
+        var source = $(templates).filter('#structure').html();
+        var template = Handlebars.compile(source);
+        var dynamic_data = {
+            title: translate.browse
+        };
+        var html = template(dynamic_data);
+
+        /* Extend default configuration. */
+        /* TODO: get it from the schema **/
+        // i.e. this.CONFIG[config.section]
+        var obj = $.extend(true, {}, this.CONFIG, {placeholder_id: '[data-role="content"]'});
+
+        switch(config.section) {
+            case "domain":
+                this.loadModule('FAOSTAT_UI_BROWSE_BY_DOMAIN', obj)
+                break;
+            case "country":
+                break;
+            case "rankings":
+                break;
+        }
+
+        /* rendering template **/
+        $placeholder.html(html);
+    };
+
+    BROWSE.prototype.loadModule = function(module, config) {
+        var _this = this;
+        Require([module], function (MODULE) {
+            _this.CONFIG.module = new MODULE();
+            _this.CONFIG.module.init(config)
+        });
+    }
+
+    BROWSE.prototype.createSubMenu = function() {
+        // TODO: create submenu with routing
+    }
+
+    BROWSE.prototype.destroy = function() {
+        // TODO: destroy
+        console.log("destroy");
+        if (this.CONFIG.module) this.CONFIG.module.destroy();
+    };
+
+    return BROWSE;
+});
